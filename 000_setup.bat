8dc5a721b43043304fb2b053dc56476035f14bdb
SHAN <ShanKonduru@gmail.com>
2025-06-15 11:05:20 +0530

    fixed setup

--- 000_setup.bat
+++ 000_setup.bat
@@ -8,20 +8,18 @@ set TRUSTED_HOSTS=--trusted-host pypi.org --trusted-host files.pythonhosted.org 
 REM Install openpyxl
 pip install %TRUSTED_HOSTS% --no-warn-script-location dotenv pytest ragas sacrebleu evaluate nltk rouge_score datasets langchain-community langchain-openai
 
-@echo off
+REM @echo off
+REM pip install --upgrade certifi
+REM REM Set the trusted hosts (important for corporate networks or SSL issues)
+REM set TRUSTED_HOSTS=--trusted-host pypi.org --trusted-host files.pythonhosted.org --trusted-host wrepp0401.cpr.ca
 
-pip install --upgrade certifi
+REM REM Set proxy variables
+REM set HTTP_PROXY=http://[username:password@]your.proxy.server:port
+REM set HTTPS_PROXY=http://[username:password@]your.proxy.server:port
 
-REM Set the trusted hosts (important for corporate networks or SSL issues)
-set TRUSTED_HOSTS=--trusted-host pypi.org --trusted-host files.pythonhosted.org --trusted-host wrepp0401.cpr.ca
+REM REM Install libraries
+REM pip install %TRUSTED_HOSTS% --no-warn-script-location dotenv pytest ragas sacrebleu evaluate nltk rouge_score datasets langchain-community langchain-openai
 
-REM Set proxy variables
-set HTTP_PROXY=http://[username:password@]your.proxy.server:port
-set HTTPS_PROXY=http://[username:password@]your.proxy.server:port
-
-REM Install libraries
-pip install %TRUSTED_HOSTS% --no-warn-script-location dotenv pytest ragas sacrebleu evaluate nltk rouge_score datasets langchain-community langchain-openai
-
-REM Unset proxy variables after use (optional)
-set HTTP_PROXY=
-set HTTPS_PROXY=
+REM REM Unset proxy variables after use (optional)
+REM set HTTP_PROXY=
+REM set HTTPS_PROXY=
